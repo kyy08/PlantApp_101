10eb773f5d620d431d6e9062a8b7832d0311d0a9
kyy08 <kyyelzz1@gmail.com>
2025-05-22 23:42:33 +0700

    membuat style constan

---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color.fromRGBO(12, 152, 105, 1);
+const kTextColor = Color(0xFF3C4046);
+const kBackgroundColor = Color(0xFFF9F8FD);
+
+const double kDefaultPadding = 20.0;
