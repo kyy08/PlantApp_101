74f2f3af855bdb7f4191783ae16a3265eff692aa
kyy08 <kyyelzz1@gmail.com>
2025-05-23 00:39:58 +0700

    menambahkan bottom navigation bar dengan ikon

---
+++ lib/components/my_bottom_nav_bar.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import '../constants.dart'; // Import ProfilPage
+
+class MyBottomNavBar extends StatelessWidget {
+  const MyBottomNavBar({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(
+        left: kDefaultPadding * 2,
+        right: kDefaultPadding * 2,
+        bottom: kDefaultPadding,
+      ),
+      height: 80,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            offset: const Offset(0, -10),
+            blurRadius: 35,
+            color: const Color.fromARGB(255, 226, 229, 228).withValues(),
+          ),
+        ],
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          IconButton(
+            icon: SvgPicture.asset("assets/icons/flower.svg"),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: SvgPicture.asset("assets/icons/heart-icon.svg"),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: SvgPicture.asset("assets/icons/user-icon.svg"),
+            onPressed: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
